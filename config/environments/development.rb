ce7555d24341334dfd007e6037f7a67832405052
Koichi Yoshizaki <kyoshi@oita-u.ac.jp>
2023-05-12 07:16:49 +0000

    Cloud9のプレビューに対応

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "53f3807f3e724d86a1a8e7c95c00b02e.vfs.cloud9.us-east-1.amazonaws.com"
 end
