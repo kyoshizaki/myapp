a51c87831f8bb3677838b5ab6e3586d5edada6bd
Koichi Yoshizaki <kyoshi@oita-u.ac.jp>
2023-05-20 02:13:50 +0000

    Scaffoled操作の取り消し

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_05_12_105926) do
-  create_table "memos", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
