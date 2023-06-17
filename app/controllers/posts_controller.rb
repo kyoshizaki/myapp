dbbe938e0ed2f89409a6f02db08f1654506f20d7
Koichi Yoshizaki <kyoshi@oita-u.ac.jp>
2023-06-17 01:20:31 +0000

    Lesson09の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,18 @@ class PostsController < ApplicationController
         @post = Post.new
     end
 
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render "new", status: :unprocessable_entity
+        end
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
+    
 end
